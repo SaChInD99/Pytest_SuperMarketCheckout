581afd0450798c8994fd139a789eb2d078556d67
SaChInD99 <sachindileepa41@gmail.com>
2025-02-16 21:45:52 +0530

    Initial commit

---
+++ TestCheckout.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    @staticmethod
+    def test_AssertTrue():
+        assert  True # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
